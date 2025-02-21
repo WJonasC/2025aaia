fa0e3f436581712a7af643c67409be1494f3be8d
WJonasC <s011421@std.hwhs.tc.edu.tw>
2025-02-21 12:06:32 +0800

    week01-1

--- week01-1.cpp
+++ week01-1.cpp
@@ -1,4 +1,6 @@
-///week01-1.cpp
+///week01-1.cpp 要自己打好全部的檔名，後面要打 .cpp
+///在瘋狂程設(練習模式)寫好的程式碼，全選，Ctrl-C
+/// CodeBlocks File-New-Empty 空白檔案，Ctrl-S存檔
 
 #include <stdio.h> ///使用 standard 標準的 input/output
 
